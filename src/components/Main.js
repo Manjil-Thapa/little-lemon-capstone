8569edecd94cf0bc10ea263360385f409fa1972e
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-12-24 23:24:59 +1000

    added about section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,13 @@
 const Main = () => {
-  return <main>Main</main>;
+  return (
+    <main>
+      <header>
+        Little Lemon is a charming neighborgood bistro that serves simple food and
+        classic cocktails in a lively but casual environment. The restaurant features
+        a locally-sourced menu with daily specials.
+      </header>
+    </main>
+  );
 };
 
 export default Main;
