8342f5f676446eb0f1e5fafa2a06e54e4b0f5772
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-12-30 23:35:49 +1000

    header component completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
       <Header />
       <Main />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className='header'>
+      <section>
+        <div className='banner'>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterraneran restaurant, focused on traditional
+            recipes served with a modern twist.
+          </p>
+          <Link to='/booking'>
+            <button aria-label='On Click'>Reserve Table</button>
+          </Link>
+        </div>
+
+        <div className='banner-img'>
+          <img src='Logo.jpg' alt='banner' />
+        </div>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
