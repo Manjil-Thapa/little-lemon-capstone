b4f56d0a2f721c7613ca53eacff3481a7e60f94d
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-12-31 01:39:05 +1000

    added logic for bookingform component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
-import Main from './components/Main';
 import Footer from './components/Footer';
 import { BrowserRouter } from 'react-router-dom';
+import BookingForm from './components/BookingForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
       <Header />
-      <Main />
+      <BookingForm />
       <Footer />
     </BrowserRouter>
   );
