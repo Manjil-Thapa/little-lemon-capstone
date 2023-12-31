f7f2adb6872c0ca66cfae1b9c3432de35d163a3b
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-12-31 15:29:27 +1000

    finalized components structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AppContainer from './components/AppContainer/AppContainer';
 import HeroSection from './components/OtherComponents/HeroSection';
 import Specials from './components/OtherComponents/Specials';
 import Testimonials from './components/OtherComponents/Testimonials';
+import About from './components/OtherComponents/About';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <HeroSection />
       <Specials />
       <Testimonials />
+      <About />
       <Footer />
     </AppContainer>
   );
--- src/components/OtherComponents/About.js
+++ src/components/OtherComponents/About.js
@@ -1,5 +1,16 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section>
+      <h2>Little Lemon</h2>
+      <h3>Chicago</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, fuga
+        fugiat. Blanditiis accusamus ad ex fugit dignissimos! Optio, ab magni
+        consectetur adipisicing elit.
+      </p>
+      <img src='hero-img.jpg' alt='placeholder' />
+    </section>
+  );
 };
 
 export default About;
