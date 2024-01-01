00a9d3eab80bac2da2f13e5f9b00df2eb497ccb8
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-02 01:15:37 +1000

    added menu array

---
+++ src/recipes.js
@@ -0,0 +1,23 @@
+const recipes = [
+  {
+    id: 1,
+    price: 12.99,
+    image: '',
+    description:
+      'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+  },
+  {
+    id: 2,
+    price: 5.99,
+    image: '',
+    description:
+      'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive salt.',
+  },
+  {
+    id: 3,
+    price: 4.99,
+    image: '',
+    description:
+      "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+  },
+];
