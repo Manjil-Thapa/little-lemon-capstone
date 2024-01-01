23b051e85eec3051d142e52dd51eb476e929fed8
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-02 01:16:55 +1000

    added menu component boilerplate

---
+++ src/components/OtherComponents/Menu.js
@@ -0,0 +1,12 @@
+const Menu = () => {
+  return (
+    <div className='menu-container'>
+      <div className='menu-header'>
+        <h2>This week's specials!</h2>
+        <button>Order Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
