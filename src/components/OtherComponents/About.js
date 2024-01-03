dec5ccf08fc99f38c07efc9dcb6099917782c168
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-04 04:43:12 +1000

    finished styling about section

--- src/components/OtherComponents/About.js
+++ src/components/OtherComponents/About.js
@@ -1,16 +1,22 @@
 const About = () => {
   return (
-    <section className='about-grid'>
-      <div className='about-container'>
-        <div className='about-left'>
-          <h2>Little Lemon</h2>
-          <h3>Chicago</h3>
-          <p>
-            Little Lemon is a charming neighborhood bistro that serves simple food
-            and classic cocktails in a lively but casual environment. The restaurant
-            features a locally-sourced menu with daily specials.
-          </p>
-        </div>
+    <section className='about'>
+      <div className='about-left'>
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>
+          Little Lemon is a charming neighborhood bistro that serves simple food and
+          classic cocktails in a lively but casual environment. The restaurant
+          features a locally-sourced menu with daily specials.
+        </p>
+        <p>
+          Little Lemon is a charming neighborhood bistro that serves simple food and
+          classic cocktails in a lively but casual environment. The restaurant
+          features a locally-sourced menu with daily specials.
+        </p>
+      </div>
+      <div className='about-right'>
+        <img src='hero-img.jpg' alt='placeholder' />
         <img src='hero-img.jpg' alt='placeholder' />
       </div>
     </section>
