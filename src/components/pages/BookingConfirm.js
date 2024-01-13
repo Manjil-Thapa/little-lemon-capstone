6af85a83e2af8993270c8d9f710fab25a7c1fda8
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-13 21:58:53 +1000

    finished styling confirmation page

--- src/components/pages/BookingConfirm.js
+++ src/components/pages/BookingConfirm.js
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const BookingConfirm = () => {
   return (
     <div className='confirm'>
-      <h2>
-        Your table has been <span>reserved</span>
-      </h2>
+      <h2>Table Reserved</h2>
+      <p>
+        Thank you for choosing <span>Little Lemon!</span> We look forward to serving
+        you a delightful dining experience.
+      </p>
+      <button>
+        <Link to='/'>RETURN TO MENU</Link>
+      </button>
     </div>
   );
 };
